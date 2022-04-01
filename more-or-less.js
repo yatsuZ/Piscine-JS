b6b59097de21ebfdcc6c8d9470cd3f4e825ef59c
Zaoui <yassine.zaoui@ynov.com>
2022-04-01 15:29:14 +0200

    more-or-less

--- more-or-less.js
+++ more-or-less.js
@@ -1,4 +1,4 @@
-const more=(nombre=Number) => nombre+1
+const more=(nombre=Number) => nombre++
 const less=(nombre=Number) => nombre--
 const add=(nombre=Number,nombre2=Number) => nombre+nombre2
 const sub=(nombre=Number,nombre2=Number) => nombre-nombre2
