68dc6d78c3bf428af2a75e44e68d124f23526367
Zaoui <yassine.zaoui@ynov.com>
2022-04-01 13:25:17 +0200

    mutability

--- mutability.js
+++ mutability.js
@@ -1,27 +1,28 @@
-/*
+
 const person = {
     name: 'Rick',
     age: 77,
     country: 'US',
   }
-*/
-const namev = person.name
-const ageClone1 = person.age +1
-const ageClone2 = person.age+1
 
 const clone1 = {
-    name:namev,
-    age:ageClone1,
-    country: 'FR'
+    name:person.name,
+    age:person.age,
+    country: person.name
 }
 const clone2 = {
-    name:namev,
-    age:ageClone2,
-    country: 'FR'
+    name:person.name,
+    age:person.age,
+    country: person.name
 }
-const samePerson= person
-/*
+const samePerson= {
+    name:person.name,
+    age:person.age,
+    country: person.name
+}
+person.age++
+person.country = "FR"
 console.log(clone1)
 console.log(clone2)
 console.log(samePerson)
-*/
+console.log(person)
