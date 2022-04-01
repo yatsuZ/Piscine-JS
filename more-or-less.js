536554f1d7a0b2dbc2ac7b4ec9da6d726c89a218
Zaoui <yassine.zaoui@ynov.com>
2022-04-01 15:52:52 +0200

    return

--- more-or-less.js
+++ more-or-less.js
@@ -1,10 +1,10 @@
 /*
-const more=(nombre) => nombre+1
+const more=(nombre) => nombre+1//equivalent en go : const more= nombre+1
 const less=(nombre) => nombre-1
 const add=(nombre,nombre2) => nombre+nombre2
 const sub=(nombre,nombre2) => nombre-nombre2
 */
-
+// ou
 function more(nombre) { return nombre+1}
 function less(nombre) { return nombre-1}
 function add(nbr1,nbr2) { return nbr1+nbr2}
---
+++ returns.js
@@ -0,0 +1,9 @@
+function id(x) {
+    return x
+}
+
+function getLength(l) {
+    return l.length
+}
+
+//console.log(getLength("0,5,6"))
