7e6c572f4fefda20938acb489a56ed1dfdf97e8d
Zaoui <yassine.zaoui@ynov.com>
2022-04-01 19:27:46 +0200

    return_0_1_[0,1]_d_une_list

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -5,6 +5,6 @@ function last(L) {
     return L[L.length-1]
 }
 function kiss(L,){
-    return [first(L)]+[last(L)]
+    return [first(L)],[last(L)]
 }
 //console.log([kiss([1, 2, 3, 4, 5, 6]) ],[6, 1])
