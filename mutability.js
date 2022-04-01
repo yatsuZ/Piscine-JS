a6822611adcfd061eb050a5fda1b26820c21e8b2
Zaoui <yassine.zaoui@ynov.com>
2022-04-01 13:27:04 +0200

    mutability

--- mutability.js
+++ mutability.js
@@ -8,12 +8,12 @@ const person = {
 const clone1 = {
     name:person.name,
     age:person.age,
-    country: person.name
+    country: person.country
 }
 const clone2 = {
     name:person.name,
     age:person.age,
-    country: person.name
+    country: person.country
 }
 const samePerson= person
 person.age++
