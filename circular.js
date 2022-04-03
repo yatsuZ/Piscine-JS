ed3731e6fa5ffd2d737bd0dd15da94d2900fa6f0
Zaoui <yassine.zaoui@ynov.com>
2022-04-03 19:37:29 +0200

    circular avec mon explication

--- circular.js
+++ circular.js
@@ -1,6 +1,8 @@
-// let circular = new Object();
-// circular.circular = circular;
-const circular = {
-    circular:circular
-}
+let circular = new Object();
+circular.circular = circular;
+//meme methode mauvaise ecriture sa ne marche pas parceque je definie un obj dans un obj 
+//en meme temps que sa creation donc non
+// const circular = {
+//     circular:circular
+// }
 //console.log(circular)
