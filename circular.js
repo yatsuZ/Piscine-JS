5a76d7bdb50b696d2e5924dd27eb93aa2fb6d2fb
Zaoui <yassine.zaoui@ynov.com>
2022-04-03 19:40:11 +0200

    circular avec mon hypothese

--- circular.js
+++ circular.js
@@ -1,8 +1,13 @@
-let circular = new Object();
-circular.circular = circular;
+// let circular = new Object();
+// circular.circular = circular;
 //meme methode mauvaise ecriture sa ne marche pas parceque je definie un obj dans un obj 
 //en meme temps que sa creation donc non
 // const circular = {
 //     circular:circular
 // }
 //console.log(circular)
+
+//donc si mon hypothese et bonne je peux le faire comme sa aussi 
+
+let circular = {};
+circular.circular = circular;
