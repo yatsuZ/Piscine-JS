7017b830d81be778a0196ca399c99b75f942ac2f
Zaoui <yassine.zaoui@ynov.com>
2022-04-07 11:34:00 +0200

    min-max

---
+++ min-max.js
@@ -0,0 +1,14 @@
+function max(a,b){
+    if (a>b){
+        return a
+    }else{
+        return b
+    }
+}
+function min(a,b){
+    if (a>b){
+        return b
+    }else{
+        return a
+    }
+}
