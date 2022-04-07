87af3c3a2d24f9e9776317a2c26bdce0cf1d0a84
Zaoui <yassine.zaoui@ynov.com>
2022-04-07 12:41:36 +0200

    is

--- is.js
+++ is.js
@@ -11,7 +11,7 @@ is.undef = (arg) => typeof(arg)==="undefined" ? true:false
 // is.def value is defined
 is.def = (arg) => typeof(arg)!="undefined" ? true:false
 // is.arr value is an array
-is.arr = (arg) => typeof(arg)==="array" ? arg:""
+is.arr = (arg) => Array.isArray(arg) ? true:false
 // is.obj value is a simple object or null objects
 is.obj = (arg) => typeof(arg)==="object" || typeof(arg)==="null objects" ? true:false
 // is.fun value is a function
