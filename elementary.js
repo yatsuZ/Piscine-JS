ebbd63d695d2b1c70c8d963f7cd3a113e75b115c
Zaoui <yassine.zaoui@ynov.com>
2022-04-08 10:42:50 +0200

    multiplication

---
+++ elementary.js
@@ -0,0 +1,19 @@
+const multiply = (a,b) =>{
+    result = 0
+    if (a<0 && b<0) {
+        for (let i=0; i>b ;i--){
+            result -= a
+        }
+    }else if (b<0) {
+        for (let i=0; i<a ;i++){
+            result += b
+        }
+    }else {
+        for (let i=0; i<b ;i++){
+            result += a
+        }
+    }
+    return result
+}
+///////////////////////////////////////////////////////
+//console.log(multiply(2,2));
