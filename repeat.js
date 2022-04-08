a7be154fe1992b3067be2061f376a07c64d19b86
Zaoui <yassine.zaoui@ynov.com>
2022-04-08 10:20:51 +0200

    repeat

--- repeat.js
+++ repeat.js
@@ -1,7 +1,19 @@
-const repeat = (S,N) => {
-    if (N == 0){
-        return ""
-    }else {
-        return S+repeat(S,N-1)
+//recurcife
+// const repeat = (S,N) => {
+//     if (N == 0){
+//         return ""
+//     }else {
+//         return S+repeat(S,N-1)
+//     }
+// }
+
+/////////////////////////////////////////////////
+
+// Iterative
+const repeat =(S,N) => {
+    var i = 0
+    while (i < N){
+        S+=S
     }
+    return S
 }
