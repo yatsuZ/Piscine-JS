4a9ec69356a0e75e72bab17a4514be50db23d59d
Zaoui <yassine.zaoui@ynov.com>
2022-04-08 12:47:41 +0200

    elementary

--- elementary.js
+++ elementary.js
@@ -49,7 +49,7 @@ const division_euclidiene=(dividende,diviseur) =>{
     if (negatif_diviseur == false && negatif_dividende == false){
         return [q,r]
     }else if( negatif_dividende || negatif_diviseur && negatif_dividende ) {
-        return[-q,-r]
+        return[q,-r]
     }else if (negatif_diviseur){
         return[-q,r]
     }
