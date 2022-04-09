75ae34b4508ed92a793a33311b536505a58d6e15
Zaoui <yassine.zaoui@ynov.com>
2022-04-10 01:15:13 +0200

    keep-cut

---
+++ keep-cut.js
@@ -0,0 +1,24 @@
+const cutFirst = (S) =>{
+    return S.slice(2)
+}
+const cutLast  = (S) =>{
+    return S.slice(0, S.length - 2)
+}
+const cutFirstLast   = (S) =>{
+    return cutFirst(cutLast(S))
+}
+const keepFirst  = (S) =>{
+    return S.slice(0,2)
+}
+const keepLast  = (S) =>{
+    return S.slice(-2)
+}
+const keepFirstLast  = (S)=>{
+    return keepFirst(S)+keepLast(S)
+}
+// console.log(cutFirst("salut"));
+// console.log(cutLast("salutm"));
+// console.log(cutFirstLast("SAsalutVA"));
+// console.log(keepFirst("SAsalutVA"));
+// console.log(keepLast("SAsalutVA"));
+// console.log(keepFirstLast("SAsalutVA"));
