86aa130902940695b5433a5ba1995520c163c498
Zaoui <yassine.zaoui@ynov.com>
2022-04-09 16:07:54 +0200

    index

--- index-of.js
+++ index-of.js
@@ -9,6 +9,18 @@ const indexOf = (L,search,starte) =>{
     }
     return -1
 }
+
+const lastIndexOf  = (L,search,starte) =>{
+    if (starte==undefined){
+        starte=0
+    }
+    for (let i =L.length; i>0;i--){
+        if (L[i]==search && i<=starte){
+            return i
+        }
+    }
+    return -1
+}
 // const a = [1,2,3,5,12]
 // const b = a.length
 // console.log(b);
