781f7e7908a2566e05c4abca6df64765dcf7506d
Zaoui <yassine.zaoui@ynov.com>
2022-04-10 22:13:48 +0200

    around

--- cut-corners.js
+++ cut-corners.js
@@ -23,22 +23,7 @@ const round = (N)=>{
     }
 }
 
-const ceil = (N)=>{
-    let symbole = 1
-    if (N<0){
-        symbole = -1
-    }
-    N= N*symbole
-    let ecart = N
-    while (ecart>=1){
-        ecart -=1 
-    }
-    if (ecart==0){
-        return N
-    }
-    ecart = 1-ecart
-    return N+(ecart*symbole)
-}
+
 
 const floor = (N)=>{
     let symbole = 1
@@ -75,11 +60,31 @@ const trunc = (N)=>{
     }
     return (N-ecart*symbole)
 }
+const ceil = (N)=>{
+    let symbole = 1
+    if (N<0){
+        symbole = -1
+    }
+    let ecart = N*symbole
+    while (ecart>=1){
+        ecart -=1 
+    }
+    if (ecart==0){
+        return N
+    }
+    if (N<0){
+        // console.log(N);
+        // console.log(ecart);
+        // console.log(N+ecart);
+        return N+ecart
+    }
+    return N+(1-ecart)
+}
 // let a = [3.141592653589793, -3.141592653589793, 2.718281828459045, -2.718281828459045, 0]
 // for (i=0;i<a.length;i++){
-//     console.log(trunc(a[i]));
+//     console.log(ceil(a[i]));
 // }
-// 3
+// 4 
 // -3
-// 2
+// 3
 // -2
