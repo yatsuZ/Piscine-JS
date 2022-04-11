cf64f6add54d6c934f7e41a5d00d8efd842e5ddf
Zaoui <yassine.zaoui@ynov.com>
2022-04-11 17:07:29 +0200

    nasa

--- nasa.js
+++ nasa.js
@@ -1,41 +1,41 @@
-// const nasa = (NBR) =>{
-//     let str=""
-//     for (let i = 1; i<=NBR;i++){
-//         if (i%3==0 && i%5==0){
-//             str+="NASA"
-//         }else if(i%3==0){
-//             str+="NA"
-//         }else if(i%5==0){
-//             str+="SA"
-//         }else{
-//             str+=i
-//         }
-//         if (i!=NBR){
-//             str+=" "
-//         }
-//     }
-//     return str
-// }
-
-const nasa = (NBR,t) =>{
-    if (t==undefined){
-        let t=1
-    }else if(t>NBR){
-        return
+const nasa = (NBR) =>{
+    let str=""
+    for (let i = 1; i<=NBR;i++){
+        if (i%3==0 && i%5==0){
+            str+="NASA"
+        }else if(i%3==0){
+            str+="NA"
+        }else if(i%5==0){
+            str+="SA"
+        }else{
+            str+=i
+        }
+        if (i!=NBR){
+            str+=" "
+        }
     }
-    let str = ""
-    if (t%3==0 && t%5==0){
-        str+="NASA"
-    }else if(t%3==0){
-        str+="NA"
-    }else if(t%5==0){
-        str+="SA"
-    }else{
-        str+=t
-    }
-    if (t==NBR){
-        return str
-    }else{
-        return str+" "+nasa(NBR,t+1)
-    }
+    return str
 }
+//sa marche pas le rec
+// const nasa = (NBR,t) =>{
+//     if (t==undefined){
+//         let t=1
+//     }else if(t>NBR){
+//         return
+//     }
+//     let str = ""
+//     if (t%3==0 && t%5==0){
+//         str+="NASA"
+//     }else if(t%3==0){
+//         str+="NA"
+//     }else if(t%5==0){
+//         str+="SA"
+//     }else{
+//         str+=t
+//     }
+//     if (t==NBR){
+//         return str
+//     }else{
+//         return str+" "+nasa(NBR,t+1)
+//     }
+// }
