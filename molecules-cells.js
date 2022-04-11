0d1c2d45ad1de0b1bb477b01e98a71002a72ae76
Zaoui <yassine.zaoui@ynov.com>
2022-04-11 18:55:50 +0200

    molecule

--- molecules-cells.js
+++ molecules-cells.js
@@ -1,7 +1,49 @@
 const RNA = (DN) =>{
-    return DN
+    let a = ""
+    if (DN[2]=="T"){
+        a +="U"
+    }else{
+        a+=DN[2]
+    }
+    if (DN[1]=="T"){
+        a +="U"
+    }else{
+        a+=DN[1]
+    }
+    if (DN[4]=="T"){
+        a +="U"
+    }else{
+        a+=DN[4]
+    }
+    if (DN[3]=="T"){
+        a +="U"
+    }else{
+        a+=DN[3]
+    }
+    return a
 }
 
 const DNA = (RN)=>{
-    return RN
+    let a = ""
+    if (RN[2]=="U"){
+        a +="T"
+    }else{
+        a+=RN[2]
+    }
+    if (RN[1]=="U"){
+        a +="T"
+    }else{
+        a+=RN[1]
+    }
+    if (RN[4]=="U"){
+        a +="T"
+    }else{
+        a+=RN[4]
+    }
+    if (RN[3]=="U"){
+        a +="T"
+    }else{
+        a+=RN[3]
+    }
+    return a
 }
