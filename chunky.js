58d414d9f3769571f967cc0e5eaf3fc94c29065d
Zaoui <yassine.zaoui@ynov.com>
2022-04-11 16:35:16 +0200

    chunky

--- chunky.js
+++ chunky.js
@@ -1,10 +1,8 @@
 const chunk = (L,diviseur) =>{
-    const NBR=Math.ceil(L.length/diviseur)
-    // console.log(NBR);
     let l = []
     let vase=[]
     for (let i = 0;i<L.length;i++){
-        if (vase.length==NBR){
+        if (vase.length==diviseur){
             l.push(vase)
             vase = []
         }
@@ -17,4 +15,4 @@ const chunk = (L,diviseur) =>{
     return l
 }
 
-// console.log(chunk(['a', 'b', 'c', 'd'], 2),"\n",[['a', 'b'], ['c', 'd']])
+// console.log(chunk(['a', 'b', 'c', 'd'], 3),"\n",[['a', 'b','c'],['d']])
