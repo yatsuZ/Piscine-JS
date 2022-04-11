3ee33794c2ec5a6aadcaa391a87f08833568c000
Zaoui <yassine.zaoui@ynov.com>
2022-04-11 16:43:07 +0200

    nasa

--- nasa.js
+++ nasa.js
@@ -1,7 +1,15 @@
 const nasa = (NBR) =>{
     let str=""
     for (let i = 0; i<NBR;i++){
-        str+=i
+        if (i%3==0 && i%5==0){
+            str+="NASA"
+        }else if(i%3==0){
+            str+="NA"
+        }else if(i%5==0){
+            str+="SA"
+        }else{
+            str+=i
+        }
         if (i!=NBR-1){
             str+=" "
         }
