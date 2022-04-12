e2dcc6177899451151f26e33ae793fccbdd8ba1a
Zaoui <yassine.zaoui@ynov.com>
2022-04-12 21:46:41 +0200

    match

--- get.js
+++ get.js
@@ -1,3 +1,10 @@
-const get = (src,path) =>{
-    return src.path
+const get = (src=Object,path=String) =>{
+    let L = []
+    for (const [key, value] of Object.entries(src)) {
+        L.push([key,value])
+      }
 }
+
+const src = { nested: { key: 'peekaboo' } }
+const path = 'nested.key'
+console.log(get(src, path) );// -> 'peekaboo'
