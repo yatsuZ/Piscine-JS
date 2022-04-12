2683a032194212879e7eb1c70f367e736e183049
Zaoui <yassine.zaoui@ynov.com>
2022-04-12 22:31:40 +0200

    match

--- its-a-match.js
+++ its-a-match.js
@@ -2,4 +2,6 @@
 let normal  = new RegExp('hi');
 let begin = new RegExp('^hi');
 let end = new RegExp('hi$');
+let beginEnd = new RegExp('^hi$');
+
 // console.log(regex.test(num)); //this will output false
