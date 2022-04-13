909c7a6f1909f3aa5299d3d6a59a0ea185773829
Zaoui <yassine.zaoui@ynov.com>
2022-04-14 01:38:16 +0200

    vowel

--- vowel-dots.js
+++ vowel-dots.js
@@ -7,12 +7,14 @@ let vowels = /[aeiou]/ig;
 const vowelDots = (s)=>{
     let l = s.match(vowels)
     let Ind =0
-    for (let i=0;i<s.length;i++){
-        if (l[Ind]==s[i]){
-            s=s.replace(l[Ind],l[Ind]+".")
-            Ind++
+    if (l!=null){
+        for (let i=0;i<s.length;i++){
+            if (l[Ind]==s[i]){
+                s=s.replace(l[Ind],l[Ind]+".")
+                Ind++
+            }
         }
     }
     return s
 }
-// console.log(vowelDots("a"));
+// console.log(vowelDots('rhythm'), 'rhythm');
