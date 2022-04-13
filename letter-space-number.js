a2e6abb51f9f7d471bda800fab4841e4b7443ee0
Zaoui <yassine.zaoui@ynov.com>
2022-04-13 15:00:44 +0200

    match

--- letter-space-number.js
+++ letter-space-number.js
@@ -2,9 +2,10 @@
 // \d=Matches digits: [0-9]
 // \W=Matches non-alphanumeric characters: [^\w]
 
-let verif=new RegExp('/\w\s\d\b/g')//ma condition Regular expression
+let verif= /\w\s\d\b/g//ma condition Regular expression
 
 const letterSpaceNumber =(S)=>{
-    return S.match(verif)
+    let l = S.match(verif)
+    return l
 }
 // console.log(letterSpaceNumber("du coup es que sa marche 1?"));//non sa marche pas >:(
