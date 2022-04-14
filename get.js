d3c3502f14be34b0ef03d342f94c654d283dc49e
Zaoui <yassine.zaoui@ynov.com>
2022-04-14 13:10:04 +0200

    same-amount

--- get.js
+++ get.js
@@ -1,10 +1,19 @@
+
 const get = (src=Object,path=String) =>{
-    let L = []
-    for (const [key, value] of Object.entries(src)) {
-        L.push([key,value])
-      }
+  // const reg = // a chaque fois qui a un point on coupe
+  let valu = undefined
+  let debut=0
+  for (let fin=0;fin<path.length;fin++){
+    if (path[fin]=="."){
+      src = src[path.slice(debut,fin)]
+      debut += fin+1
+    }else if (fin==path.length){
+      src= src[path.slice(debut,fin)]
+    }
+  }
+  return src
 }
 
-const src = { nested: { key: 'peekaboo' } }
-const path = 'nested.key'
-console.log(get(src, path) );// -> 'peekaboo'
+// const src = { nested: { key: 'peekaboo' } }
+// const path = 'nested.key'
+// console.log(get(src, path) );// -> 'peekaboo'
