9a01685f6f720031fb4e0ef3cc69f929480578e7
Zaoui <yassine.zaoui@ynov.com>
2022-04-14 02:19:33 +0200

    same-amount

--- same-amount.js
+++ same-amount.js
@@ -3,7 +3,7 @@ const sameAmount = (s,regx1,regx2)=>{
     const b =s.match(regx2)
     // console.log(a);
     // console.log(b);
-    return (a==b)
+    return (a,b)
 }
 
 // const data ="hello how are you hello how are you hello how are you qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd"
