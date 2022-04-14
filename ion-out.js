32ffad197ff412a51e3ce06ac0866b1b05f700c2
Zaoui <yassine.zaoui@ynov.com>
2022-04-14 17:00:49 +0200

    ion

--- ion-out.js
+++ ion-out.js
@@ -1,17 +1,32 @@
-const r = /iont/g
+const r = /ion/g
 
 const ionOut = (S) =>{
     let debut =0
     let s = ""
     let l=[]
+    let b =""
     for (let fin = 0;fin<S.length;fin++){
         if (S[fin]==" "){
             s = S.slice(debut,fin)
             debut=fin+1
             if (r.test(s)){
-                l.push(s.replace(r,"t"))
+                b=s.replace(r,"")
+                l.push(b)
+            }
+            else {
+                l.push(s)
             }
         }
     }
+    s = S.slice(debut,S.length)
+    if (r.test(s)){
+        b=s.replace(r,"")
+        l.push(b)
+        }
+    else {
+        l.push(s)
+        }
     return l 
 }
+// console.log();
+// console.log(ionOut('attention, direction'))
