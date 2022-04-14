12175c1c860f6bc4194028fa84f430e49d254810
Zaoui <yassine.zaoui@ynov.com>
2022-04-14 02:17:40 +0200

    same-amount

--- same-amount.js
+++ same-amount.js
@@ -1,9 +1,9 @@
 const sameAmount = (s,regx1,regx2)=>{
     const a =s.match(regx1).length
     const b =s.match(regx2).length
-    // console.log(a);
+    console.log(a);
     return (a==b)
 }
 
-// const data ="hello how are you hello how are you hello how are you qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd"
-// console.log(sameAmount(data, /h/, /w/));
+const data ="hello how are you hello how are you hello how are you qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd qqqqqqq q qqqqqqqfsqqqqq q qq  qw w wq wqw  wqw ijnjjnfapsdbjnkfsdiqw klfsdjn fs fsdnjnkfsdjnk sfdjn fsp fd"
+console.log(sameAmount(data, /h/, /w/));
