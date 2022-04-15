ad781a0de445c5d42c857ab14b7163656f9a743d
Zaoui <yassine.zaoui@ynov.com>
2022-04-15 13:44:50 +0200

    dr-strange debut du mcu

--- dr-strange.js
+++ dr-strange.js
@@ -2,23 +2,24 @@ const semaine = [
     "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday",
     "secondMonday","secondTuesday","secondWednesday","secondThursday","secondFriday","secondSaturday","secondSunday",
 ]
+
 const addWeek = (s)=>{
-    const ajd= ((s.getMonth()*30+s.getDate())%14)-1 
-    // console.log(ajd);
+    let ajd= (s.getDate()-1)%14
+    if (s.getFullYear()>=2000){
+        ajd-=3
+    }
     for (let  i = 0;i<semaine.length;i++){
         if (i==ajd){
             return semaine[i]
-        }else if (ajd==-1){
-            return semaine[13]
         }
     }
 }
 // const monday = new Date('0001-01-01')
-// console.log(monday.getDate(),monday.getMonth(),monday.getFullYear());
+// console.log(monday.getFullYear(),"m",monday.getMonth(),"m",monday.getDate(),"m",monday.getDay(),"m",monday.getSeconds());
 // console.log(addWeek(monday));
-// const wensday = new Date('0001-01-03')
-// console.log(wensday.getDate(),wensday.getMonth(),wensday.getFullYear());
+// const wensday = new Date('2000-01-03')
+// console.log(wensday.getDate(),wensday.getMonth(),wensday.getFullYear(),wensday.getSeconds());
 // console.log(addWeek(wensday));
-const wensday = new Date('0001-01-03')
-console.log(wensday.getDate(),wensday.getMonth(),wensday.getFullYear());
-console.log(addWeek(wensday));
+// const secondMonday = new Date('2025-08-11')
+// console.log(secondMonday.getDate(),secondMonday.getMonth(),secondMonday.getFullYear(),secondMonday.getSeconds());
+// console.log(addWeek(secondMonday));
