fee45f661407e9fccaf07cb506ec23707d8863c9
Zaoui <yassine.zaoui@ynov.com>
2022-04-15 12:28:59 +0200

    dr-strange debut du mcu

--- dr-strange.js
+++ dr-strange.js
@@ -1,10 +1,10 @@
 const semaine = [
-    "Monday","Tuesday","Wednesd","Thursday","Friday","Saturday","Sunday",
+    "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday",
     "secondMonday","secondTuesday","secondWednesd","secondThursday","secondFriday","secondSaturday","secondSunday",
 ]
 const addWeek = (s)=>{
     const ajd= ((s.getMonth()*30+s.getDate())%14)-1 
-    console.log(ajd);
+    // console.log(ajd);
     for (let  i = 0;i<semaine.length;i++){
         if (i==ajd){
             return semaine[i]
@@ -13,6 +13,9 @@ const addWeek = (s)=>{
         }
     }
 }
-const s = new Date('0001-01-01')
-console.log(s.getDate(),s.getMonth(),s.getFullYear());
-console.log(addWeek(s));
+// const monday = new Date('0001-01-01')
+// console.log(monday.getDate(),monday.getMonth(),monday.getFullYear());
+// console.log(addWeek(monday));
+// const wensday = new Date('0001-01-03')
+// console.log(wensday.getDate(),wensday.getMonth(),wensday.getFullYear());
+// console.log(addWeek(wensday));
