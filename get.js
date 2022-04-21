97aa8cd671a1eb2bdad6611135fd02d8a73b5bc9
Zaoui <yassine.zaoui@ynov.com>
2022-04-21 11:40:00 +0200

    using map

--- get.js
+++ get.js
@@ -1,30 +1,20 @@
 const arr = (arg) => Array.isArray(arg) ? true:false
 const get = (src=Object,path=String) =>{
-  // const reg = // a chaque fois qui a un point on coupe
-  let trouver=false
-  let debut=0
-  for (let fin=0;fin<path.length;fin++){
+  let transfer = ""
+  let debut = 0
+  let  b = undefined
+  for (let fin =0;fin<path.length;fin++){
     if (path[fin]=="."){
-      if (arr(src)){
-        console.log("salut");
+      transfer = path.slice(debut,fin)
+      if (b==undefined){
+        b = src[path]
       }else{
-        // console.log(path.slice(debut,fin));
-        for (let [key] of Object.entries(src)){
-          trouver= false
-          if (path.slice(debut,fin)==key){
-            src = src[path.slice(debut,fin)]
-            debut += fin+1
-            trouver=true
-            break
-          }
-        }
-        if (trouver==false){
-          return undefined
-        }
+        b = b[path]
       }
+      debut += fin+1
     }
   }
-  return typeof(src[path.slice(debut,path.length)])==undefined?undefined:src[path.slice(debut,path.length)]
+  return b
 }
 
 const t =(a)=>a+1
--- using-map.js
+++ using-map.js
@@ -1,10 +1,15 @@
 const citiesOnly = (L) =>{
     let l = []
-    for (let i =0;i<L.length;i++){
-        l.push(L[i].city)
-    }
-    if (l==[]){
-        return L
-    }
+    l = L.map(x=> x.city )
     return l
 }
+// console.log(citiesOnly([
+//     {
+//       city: 'Los Angeles',
+//       temperature: '  101 °F   ',
+//     },
+//     {
+//       city: 'San Francisco',
+//       temperature: ' 84 ° F   ',
+//     },
+//   ])) // -> ['Los Angeles', 'San Francisco']
